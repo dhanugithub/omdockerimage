749133307496ec959af122a52aa2887a63dd5bdc
dhanugithub <37544947+dhanugithub@users.noreply.github.com>
2018-08-10 11:01:00 +0200

    Update om_install.sh

--- scripts/om_install.sh
+++ scripts/om_install.sh
@@ -18,9 +18,9 @@ fi
 
 sudo chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql start
 
-mysql -u ubuntu -p${DB_ROOT_PASS} -se "CREATE DATABASE ${OM_DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
-mysql -u ubuntu -p${DB_ROOT_PASS} -se "CREATE USER '${OM_DB_USER}'@'localhost' IDENTIFIED BY '${OM_DB_PASS}'"
-mysql -u ubuntu -p${DB_ROOT_PASS} -se "grant all privileges on ${OM_DB_NAME}.* to '${OM_DB_USER}'@'localhost'"
+mysql -u root -p${DB_ROOT_PASS} -se "CREATE DATABASE ${OM_DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
+mysql -u root -p${DB_ROOT_PASS} -se "CREATE USER '${OM_DB_USER}'@'localhost' IDENTIFIED BY '${OM_DB_PASS}'"
+mysql -u root -p${DB_ROOT_PASS} -se "grant all privileges on ${OM_DB_NAME}.* to '${OM_DB_USER}'@'localhost'"
 
 sudo chown -R nobody:nogroup ${OM_HOME}
 sudo -u nobody ./admin.sh -v -i -tz Europe/Berlin -email kulkarnidhana22@gmail.com -group group \
