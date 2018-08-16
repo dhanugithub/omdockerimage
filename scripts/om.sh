6ea98eb00640a43542a436cc4229e4ccd4123bac
dhanugithub <37544947+dhanugithub@users.noreply.github.com>
2018-08-16 13:49:43 +0200

    Update om.sh

--- scripts/om.sh
+++ scripts/om.sh
@@ -13,6 +13,6 @@
 # #############################################
 
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql start
-#sudo chmod 755 ${OM_HOME}/red5-debug.sh
+
 sudo -u nobody ./bin/catalina.sh run
 
